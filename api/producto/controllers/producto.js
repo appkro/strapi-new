f223d6f2d7006b9a9141e2c7d457edcea21b1b7a
jealviakronio <app@kronio.io>
2021-10-06 10:22:18 -0500

    Prueba de controller con politicas

--- api/producto/controllers/producto.js
+++ api/producto/controllers/producto.js
@@ -9,18 +9,20 @@ module.exports = {
 
   async findMe(ctx) {
     let entities;
-    console.log("----------------");
-    /* if (ctx.query._q) {
-      entities = await strapi.services.producto.search(ctx.query);
+    console.log(ctx.state.user);
+    if (ctx.query._q) {
+      entities = await strapi.services.prueba.search(ctx.query);
     } else {
-      entities = await strapi.services.producto.find(ctx.query);
+      entities = await strapi.services.prueba.find(ctx.query);
     }
-
-    return entities.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models.producto })
-    ); */
-    return {
-      hola: "mundo",
-    };
+    //console.log(await strapi.query('prueba').findOne({ id: 1 }))
+    const data = entities.map((z) => {
+      return {
+        id: z.id,
+        name: z.peuebua,
+        hola: z.hola,
+      };
+    });
+    return data;
   },
 };
